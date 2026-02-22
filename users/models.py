7e3a9cf5ba7fa07bbcb73d1274f4d7d1de7500ba
sharaffatima <sharaffatima000@gmail.com>
2026-02-22 15:34:12 +0100

    Remove unused Import

--- users/models.py
+++ users/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from rbac.models import Role
 
 
 class User(AbstractUser):
@@ -39,7 +38,7 @@ class User(AbstractUser):
     )
 
     roles = models.ManyToManyField(
-        'rbac.Role',
+        "rbac.Role",
         through='rbac.UserRole',
         through_fields=('user', 'role'),
         related_name='users'
