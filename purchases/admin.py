c9678504335086d5953f0d567a3e0cdce564563c
sharaffatima <sharaffatima000@gmail.com>
2026-02-26 13:58:03 +0100

    Register missing tables

--- purchases/admin.py
+++ purchases/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import PurchaseHistory
+from .models import PurchaseHistory, PurchaseProposal, PurchaseProposalItem
 # Register your models here.
 
 admin.site.register(PurchaseHistory)
+admin.site.register(PurchaseProposal)
+admin.site.register(PurchaseProposalItem)
